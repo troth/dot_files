495dd99d1e587da68522dd70ae08771827d5b8bd
Theodore A. Roth <theodore.a.roth@hp.com>
2011-05-02 08:42:14 -0600

    Python mode tweaks.

--- .emacs.d/init-python-mode.el
+++ .emacs.d/init-python-mode.el
@@ -1,12 +1,14 @@
+;;; Python-mode does play nice with CEDET. :-(
 ;;;(require 'pycomplete)
-;;;(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-;;;(autoload 'python-mode "python-mode" "Python editing mode." t)
+;(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+;(setq interpreter-mode-alist(cons '("python" . python-mode)
+;                             interpreter-mode-alist))
+;(autoload 'python-mode "python-mode" "Python editing mode." t)
+
 ;;;(autoload 'pymacs-load "pymacs" nil t)
 ;;;(autoload 'pymacs-eval "pymacs" nil t)
 ;;;(autoload 'pymacs-apply "pymacs")
 ;;;(autoload 'pymacs-call "pymacs")
-;;;(setq interpreter-mode-alist(cons '("python" . python-mode)
-;;;                             interpreter-mode-alist))
 
 ;(defun my-python-hook ()
   ;; my customizations for all of python and related modes
