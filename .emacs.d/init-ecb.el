d7ca0df856c69f1fe196d31467e2419885d00bec
Theodore A. Roth <theodore.a.roth@hp.com>
2011-05-02 08:45:46 -0600

    Add note about ecb symlink.

--- .emacs.d/init-ecb.el
+++ .emacs.d/init-ecb.el
@@ -2,7 +2,10 @@
 ;;; ECB (Emacs Code Browser)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;; The path to ecb is a symlink to the actual soruce which is usually
+;;; a git repo.
 (add-to-list 'load-path "~/.emacs.d/site-el/ecb")
+
 (require 'ecb)
 ;(require 'ecb-autoloads)
 (setq ecb-tip-of-the-day nil)
