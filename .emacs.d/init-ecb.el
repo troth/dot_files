2fbbe7f4853a77f7a7f74ef76525abb86430d095
Theodore A. Roth <theodore.a.roth@hp.com>
2011-05-02 08:40:51 -0600

    Change load path for ecb.

--- .emacs.d/init-ecb.el
+++ .emacs.d/init-ecb.el
@@ -2,9 +2,21 @@
 ;;; ECB (Emacs Code Browser)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-to-list 'load-path "~/.emacs.d/ecb-git")
+(add-to-list 'load-path "~/.emacs.d/site-el/ecb")
 (require 'ecb)
 ;(require 'ecb-autoloads)
 (setq ecb-tip-of-the-day nil)
 ;(setq ecb-compile-window-height 20)
 
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
