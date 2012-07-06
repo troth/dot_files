7f5a1dfc8c4ac87b7e55742b7fbc819c94d077c7
Theodore A. Roth <troth@cardinalpeak.com>
2012-07-06 09:25:19 -0600

    EMACS: Add speedbar configuration.
    Found a nice script which puts the speedbar into the window frame insted
    of opening another window frame.
    
        http://www.emacswiki.org/SrSpeedbar

---
+++ _emacs.d/init-speedbar.el
@@ -0,0 +1,37 @@
+;;;
+;;; Setup SpeedBar
+;;;
+
+;;; Put the speedbar on the left side.
+(setq sr-speedbar-right-side nil)
+
+;;; Usage:
+;;;
+;;;    “sr-speedbar-open”
+;;;
+;;;    Open sr-speedbar window.
+;;;
+;;;    “sr-speedbar-close”
+;;;
+;;;    Close sr-speedbar window.
+;;;
+;;;    “sr-speedbar-toggle”
+;;;
+;;;    Toggle sr-speedbar window.
+;;;
+;;;    “sr-speedbar-select-window”
+;;;
+;;;    Select sr-speedbar window.
+;;;
+;;;    “sr-speedbar-refresh-turn-on”
+;;;
+;;;    Turn on refresh speedbar content.
+;;;
+;;;    “sr-speedbar-refresh-turn-off”
+;;;
+;;;    Turn off refresh speedbar content.
+;;;
+;;;    “sr-speedbar-refresh-toggle”
+;;;
+;;;    Toggle refresh speedbar content.
+(require 'sr-speedbar)
