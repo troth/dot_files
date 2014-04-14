254ff2429d3bf089eca714ed529fc819cf01e3cf
Theodore A. Roth <troth@openavr.org>
2014-04-14 13:44:29 -0600

    emacs: Remove safe-local-variable-values from custom.
    These conflict with some c-mode styles.

--- _emacs.d/init-custom.el
+++ _emacs.d/init-custom.el
@@ -4,7 +4,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.40")
- '(safe-local-variable-values (quote ((eval progn (c-set-offset (quote case-label) 3) (c-set-offset (quote substatement-open) 0) (c-set-offset (quote statement-case-open) 0))))))
+)
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
