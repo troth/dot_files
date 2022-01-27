b07eb972099e6c6d0d2e3b3c9e490eb7b78ed5a1
Theodore A. Roth <troth@openavr.org>
2022-01-27 08:09:39 -0700

    nvim: Add NERDTree plugin
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- cfg_nvim/lua/plugins.lua
+++ cfg_nvim/lua/plugins.lua
@@ -17,6 +17,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/nvim-cmp'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip'
+  use 'preservim/nerdtree'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
