9390013c516dc9fcebfa07b2bf1f7b2da5dd3f4d
Theodore A. Roth <troth@openavr.org>
2023-01-08 07:13:29 -0700

    nvim: Update Packer configuration
    Use same configuration setup code as now shown in Packer documentation.
    
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- cfg_nvim/lua/plugins.lua
+++ cfg_nvim/lua/plugins.lua
@@ -1,23 +1,39 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
       'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path
-  })
+    })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   -- Add plugins here
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
-  use 'preservim/nerdtree'
+  use('wbthomason/packer.nvim')
+
+  -- LSP Support
+  use('neovim/nvim-lspconfig')
+
+  -- Autocompletion
+  use('hrsh7th/cmp-nvim-lsp')
+  use('hrsh7th/cmp-buffer')
+  use('hrsh7th/cmp-path')
+  use('hrsh7th/cmp-cmdline')
+  use('hrsh7th/nvim-cmp')
+  use('saadparwaiz1/cmp_luasnip')
+
+  -- Snippets
+  use('L3MON4D3/LuaSnip')
+
+  -- File Navigation
+  use('preservim/nerdtree')
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
